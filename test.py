284e7a243187f88bce46693f32aea9c77c7c41a4
Endri Kacupaj <endrikacupaj@gmail.com>
2020-10-09 10:11:03 +0200

    - add rename inference to test

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
 logger = logging.getLogger(__name__)
 
 # set device
-torch.cuda.set_device(3)
+torch.cuda.set_device(2)
 
 # set a seed value
 random.seed(args.seed)
