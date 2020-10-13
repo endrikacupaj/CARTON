fc0985c9011db6c13bbd5f43549fb298fa487d71
Endri Kacupaj <endrikacupaj@gmail.com>
2020-10-13 22:25:56 +0200

    - small fix

--- test.py
+++ test.py
@@ -14,13 +14,10 @@ logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
                     datefmt='%d/%m/%Y %I:%M:%S %p',
                     level=logging.INFO,
                     handlers=[
-                        logging.FileHandler(f'{args.path_results}/inference_{args.question_type}.log', 'w'),
+                        logging.FileHandler(f'{args.path_results}/test_{args.question_type}.log', 'w'),
                         logging.StreamHandler()
                     ])
 logger = logging.getLogger(__name__)
-
-# set device
-torch.cuda.set_device(2)
 
 # set a seed value
 random.seed(args.seed)
